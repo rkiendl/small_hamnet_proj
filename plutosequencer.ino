21852621de65a3e2423b2a445141365680e25d23
Robert Kiendl OE6RKE <robert.kiendl@automatis.com>
2020-06-09 23:20:33 +0200

    Add files via upload
    Pluto Sequencer for arduino nano and bistable relays

---
+++ plutosequencer.ino
@@ -0,0 +1,88 @@
+//pluto sequencer to control hf relais and hf amplifyer
+int pinRelais1 = 2; //D2
+int pinRelais2 = 3; //D3
+int pinRelais3 = 4; //D4
+int pinRelais4 = 5; //D5
+int pinPTT = 7; //D7 as in
+
+bool isPTTOn = false;
+bool isPTTOff = false;
+bool lastState = false;
+
+void setup() {
+  //init input
+  pinMode(pinPTT, INPUT_PULLUP);
+  
+  // init outputs
+  pinMode(pinRelais1, OUTPUT);
+  pinMode(pinRelais2, OUTPUT);
+  pinMode(pinRelais3, OUTPUT);
+  pinMode(pinRelais4, OUTPUT);
+
+  digitalWrite(pinRelais1, HIGH);
+  digitalWrite(pinRelais2, HIGH);
+  digitalWrite(pinRelais3, HIGH);
+  digitalWrite(pinRelais4, HIGH);
+
+  //bring relais defined state, set relay pin 1
+  digitalWrite(pinRelais1, LOW);
+  delay(200);
+  digitalWrite(pinRelais1, HIGH);
+
+  //init serial
+  Serial.begin(9600);
+  Serial.println("start---");     
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  //valPTT = digitalRead(pinPTT);
+  //Serial.println(valPTT);
+
+  //0 am pin is pressed, 1 am pin is released
+  if(digitalRead(pinPTT) == false) { //entprellen
+    delay(100);
+    
+    if(digitalRead(pinPTT) == false) { //0 = pressed
+      //Serial.print("PTT ON\r\n");
+      
+      //PTT Off -> On
+      if(isPTTOn == false) {
+        Serial.print("Relais ON\r\n");
+        
+        digitalWrite(pinRelais2, LOW);
+        delay(200);
+        digitalWrite(pinRelais2, HIGH);
+  
+        //power via relay 3
+        digitalWrite(pinRelais3, LOW);;        
+        
+        isPTTOn = true;
+        isPTTOff = false;
+      }
+      lastState = true;
+   
+    }
+  }
+  else {
+    lastState = false;
+  }
+  //PTT On -> Off
+  if(isPTTOff == false && lastState == false) {
+    Serial.print("Relais OFF\r\n");
+    
+    digitalWrite(pinRelais1, LOW);
+    delay(200);
+    digitalWrite(pinRelais1, HIGH);
+      
+    //power via relay 3
+    digitalWrite(pinRelais3, HIGH);
+    
+    isPTTOff = true;
+    isPTTOn = false;
+  }
+  
+  //some delay
+  delay(200);//delay to keep stuff smooth
+}
